53e1154d6dbba2659c970298f3be833e1f2b194a
unknown <davidkalatzis@gmail.com>
2020-03-05 22:32:45 +0100

    Updated the main menu visuals.

---
+++ Assets/Scripts/UIScripts/MenuButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    [SerializeField] private Animator _animator;
+
+
+    public void OnEnterSelect()
+    {
+        _animator.SetBool("IsSelected", true);
+        _text.color = Color.black;
+    }
+
+    public void OnExitSelect()
+    {
+        _animator.SetBool("IsSelected", false);
+        _text.color = Color.white;
+    }
+}
