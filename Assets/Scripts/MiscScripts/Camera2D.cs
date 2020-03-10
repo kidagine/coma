b3710ab7c4d11307ac9df58dc83c76c5e6548280
unknown <davidkalatzis@gmail.com>
2020-03-11 00:07:28 +0100

    Setting up basis for camera movement.

---
+++ Assets/Scripts/MiscScripts/Camera2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2D : MonoBehaviour
+{
+    private CameraBorder _cameraBorder;
+    private float _speed = 2.5f;
+    private float _smoothedPositionX;
+    private float _smoothedPositionY;
+    private bool _isFollowingPlayer;
+
+
+    void Update()
+    {
+        transform.position +=  Vector3.right * Time.deltaTime * 3f;
+    }
+
+    public void SetFollowPlayer(bool isFollowingPlayer, CameraBorder cameraBorder)
+    {
+        _smoothedPositionX = transform.position.x;
+        _smoothedPositionY = transform.position.y;
+        _cameraBorder = cameraBorder;
+        _isFollowingPlayer = isFollowingPlayer;
+    }
+}
---
+++ Assets/Scripts/MiscScripts/Camera2DBorder.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public enum CameraBorder { TopBorder, BottomBorder, RightBorder, LeftBorder };
+public class Camera2DBorder : MonoBehaviour
+{
+    [SerializeField] private Camera2D _camera2D;
+    [SerializeField] private CameraBorder _cameraBorder;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            _camera2D.SetFollowPlayer(collision.transform, _cameraBorder);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            _camera2D.SetFollowPlayer(false, _cameraBorder);
+        }
+    }
+}
