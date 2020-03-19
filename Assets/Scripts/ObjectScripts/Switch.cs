e1011fd5eddf1534ebe2d8eae9add3bb430c3c31
unknown <davidkalatzis@gmail.com>
2020-03-19 14:30:23 +0100

    Added a switch to trigger environment events.

---
+++ Assets/Scripts/ObjectScripts/Switch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField] private Animator _activatorAnimator;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Sprite _openSwitchSprite;
+    [SerializeField] private Sprite _closedSwitchSprite;
+    [SerializeField] private bool _isClosed;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_isClosed)
+        {
+            _activatorAnimator.SetTrigger("ShowUp");
+            _spriteRenderer.sprite = _openSwitchSprite;
+        }
+    }
+}
