ec835bdffa3f293dec0b3daaeeba8ecbf2a8c2b3
unknown <davidkalatzis@gmail.com>
2020-03-23 22:42:55 +0100

    Minor additions to the visuals.

---
+++ Assets/Scripts/EnvironmentScripts/Water.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _timeUntilActivate;
+  
+    void Start()
+    {
+        Invoke("EnableAnimator", _timeUntilActivate);    
+    }
+
+    private void EnableAnimator()
+    {
+        _animator.enabled = true;
+    }
+}
